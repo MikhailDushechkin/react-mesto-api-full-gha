4e613b9e77b50492802ba9134ef7745780ac3130
Mikhail Dushechkin <mishan92097@gmail.com>
2023-05-12 16:39:35 +0300

    refactor: add cors, dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
@@ -9,13 +11,21 @@ const responseError = require('./middlewares/response');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cors());
+
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/mestodbnew', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.use(express.json());
 app.use(helmet());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(mainRouter);
 
