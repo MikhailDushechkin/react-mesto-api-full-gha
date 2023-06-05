8ebd3182704c9119b28b58f0a752a021a20d873a
Mikhail Dushechkin <mishan92097@gmail.com>
2023-06-05 16:29:27 +0300

    fix: changed api port

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const mainRouter = require('./routes/index');
 const responseError = require('./middlewares/response');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 const allowedCors = [
@@ -17,7 +17,7 @@ const allowedCors = [
   'http://dmm.front.mesto.nomoredomains.monster',
   'http://dmm.back.mesto.nomoredomains.monster',
   'https://dmm.front.mesto.nomoredomains.monster',
-  'localhost:3000',
+  'localhost:3001',
 ];
 // eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
